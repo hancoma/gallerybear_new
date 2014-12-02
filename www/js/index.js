525d15a7c4a3350fd224f91e40ced2698cbc26ef
hancoma <hancoma@gmail.com>
2014-12-02 20:08:59 +0900

    원상태

--- www/js/index.js
+++ www/js/index.js
@@ -37,7 +37,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        PhoneGap.UsePolling=true;
+     
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
